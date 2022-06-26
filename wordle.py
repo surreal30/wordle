3ac9718cdabf0230ea08efe35b19bb1f79163cf2
pawann-2000 <pawann931@gmail.com>
2022-06-27 02:00:04 +0530

    minor changes

--- wordle.py
+++ wordle.py
@@ -1,14 +1,20 @@
+# WORDLE IMPLEMENTED IN PYTHON
+
 import getpass
 import emoji
+
 answer_word = getpass.getpass("Word of the day: ")
 word_length = len(list(answer_word))
 l1 = list(answer_word)
+
 print("""Rules:
 1. You have total 6 tries to guess the word correctly.
 2. If the letter of the word is correct at the corresponsing place of the answer. It will print """, emoji.emojize(':green_square:'), """ at that place.
 3. If the letter is wrong it will print """, emoji.emojize(':brown_square:'), """.
 """)
+
 print("The word is of length ", word_length, ". All the best!")
+
 for i in range(1, word_length+1):
 	try_word = input("Enter your try:  ")
 	l2 = list(try_word)
@@ -26,5 +32,6 @@ for i in range(1, word_length+1):
 		break
 	else:
 		print("Oops! Something is not quite right! Try again ", ' '.join(map(str, trial)))
+
 if(fg!=word_length):
 	print("Better luck next time. The word is: ", answer_word)
